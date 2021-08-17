77a24d5e4a053e8120abe0d70eb101be4fb40ee6
gtrman97 <andrew.killian@g.austincc.edu>
2021-08-17 11:59:39 -0500

    added weather data

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let queryUrl = "https://api.openweathermap.org/data/2.5/onecall?"; 
 let lat = "lat=30.266666&"; 
 let lon = "lon=-97.733330&"; 
-let apiOptions = "exclude=minutely,alerts&";
+let apiOptions = "units=imperial&exclude=minutely,alerts&";
 let apiKey = "appid=dc2c6c767a3bc108510c5151248c8456"; 
 let file = queryUrl + lat + lon + apiOptions + apiKey;
 
@@ -16,6 +16,11 @@ fetch(file)
     let temp = Math.round(data.current.temp);
     let pressure = data.current.pressure;
     let humidity = data.current.humidity; 
+
+    document.getElementById('today').innerHTML = description;
+    console.log(data);
+    document.getElementById('weather').innerHTML = temp;
+
 
     // Weather hourly data 
     let hourNow = data.hourly[0].temp;
