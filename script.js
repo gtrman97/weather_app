4e3d736a66bb0a75e9dceb2967651b032eaca33d
gtrman97 <andrew.killian@g.austincc.edu>
2021-08-17 19:42:07 -0500

    added space

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ fetch(file)
 
     document.getElementById('today').innerHTML = description;
     console.log(data);
-    document.getElementById('weather').innerHTML = temp + '°F';
+    document.getElementById('weather').innerHTML = temp + ' °F';
 
 
     // Weather hourly data 
