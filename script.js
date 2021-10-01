3d838455ee7d10734a45a7a2223839e54f30daf2
gtrman97 <andrew.killian@g.austincc.edu>
2021-10-01 14:10:19 -0500

    deleted comment

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ fetch(file)
 .then((data) => {
 
     // Weather main data
-    //comment 
     let main = data.current.weather[0].main; 
     let description = data.current.weather[0].description;
     description = titleCase(description); 
